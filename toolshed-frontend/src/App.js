0287c23080a7285d2ad59bb31c264cb6f3d6d534
xjeffss <elmorejeffreyp@johndeere.com>
2021-03-10 17:10:32 -0600

    much imported from wayfarer

--- toolshed-frontend/src/App.js
+++ toolshed-frontend/src/App.js
@@ -30,6 +30,17 @@ class App extends Component {
     return (
       <div className="App">
         Neighborhood Toolshed
+        <h3>Please fill in the information below to Register</h3>
+            <form id="login" action="/users" method="POST">
+                Name: <input type="text" name="firstName" defaultValue="First"/><input type="text" name="lastName" defaultValue="Last"/><br/>
+                Username: <input type="text" name="username" /><br/>
+                Password: <input type="text" name="password" /><br/>
+                Email (for notifications):<input type="email" name="email" defaultValue="optional"/><br/>
+
+                <input type="submit" name="" value="Submit"/>
+            </form>
+      <br></br>
+
         <form onSubmit={this.login}>
           <input
             name='username'
