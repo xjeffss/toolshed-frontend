c6c9e0eb6e2dbd807a41efadfb7cb706f3474705
xjeffss <elmorejeffreyp@johndeere.com>
2021-03-10 08:24:56 -0600

    models created

--- toolshed-frontend/src/App.js
+++ toolshed-frontend/src/App.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Neighborhood Toolshed
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+  loginOnChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  login = async (e) => {
+    e.preventDefault();
+    const data = {
+      username: this.state.username,
+      password: this.state.password,
+    };
+    console.log(data);
+    const response = await axios.post('http://localhost:3001/auth/login', data);
+    console.log(response);
+  };
+  render(){
+    return (
+      <div className="App">
+        Neighborhood Toolshed
+        <form onSubmit={this.login}>
+          <input
+            name='username'
+            type='text'
+            placeholder='username'
+            value={this.state.username}
+            onChange={this.loginOnChange}
+          />
+          <input
+            name='password'
+            type='password'
+            placeholder='password'
+            value={this.state.password}
+            onChange={this.loginOnChange}
+          />
+          <input type='submit' value='Login' />
+        </form>
+      </div>
+    )
+  }
+  ;
 }
 
 export default App;
